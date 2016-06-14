4fd7f328fca3f9566dfac6519a817f5d1d595b1e
Vadym Stasyuk <stasyukvad@ukr.net>
2016-06-14 21:14:18 +0300

    msdm

---
+++ src/stasyuk/goit/module4/task1/NegativeValueException.java
@@ -0,0 +1,7 @@
+package stasyuk.goit.module4.task1;
+
+/**
+ * Created by V@D on 12.06.2016.
+ */
+public class NegativeValueException {
+}
---
+++ src/stasyuk/goit/module6/ExceptionExampleRunner.java
@@ -0,0 +1,7 @@
+package stasyuk.goit.module6;
+
+/**
+ * Created by V@D on 11.06.2016.
+ */
+public class ExceptionExampleRunner {
+}
